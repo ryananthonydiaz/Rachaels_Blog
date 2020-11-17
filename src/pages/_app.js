f04c894b15cc8a394c5482782c77e0e95dbdcece
Ryan Diaz <email@ryananthonydiaz.com>
2020-11-16 18:47:32 -0800

    Title Changed

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -17,7 +17,7 @@ export default function App({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <title>My page</title>
+        <title>RVCH</title>
         <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
       </Head>
       <ThemeProvider theme={theme}>
